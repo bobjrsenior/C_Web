b8d365ba984370df0e696a3c3c705c77e3961d01
Bobjrsenior <bobjrsenior@gmail.com>
2015-12-03 22:54:44 +0000

    Added main.c with a few includes. Also added gitignore for swp files

---
+++ src/main.c
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  A simple webserver written in C. The goal is to be able to handle
+ *                  http requests safely.
+ *
+ *        Version:  1.0
+ *        Created:  12/03/15 22:45:39
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Bobjrsenior (BOB), bobjrsenior@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  Initializes the program
+ * =====================================================================================
+ */
+int main ( int argc, char *argv[] ){
+	
+	return EXIT_SUCCESS;
+}/* ----------  end of function main  ---------- */
